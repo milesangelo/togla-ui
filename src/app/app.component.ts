90b29572bf6db5ffe91c3d36a85539de5251397b
milesangelo <milesvendetti@gmail.com>
2020-12-06 21:16:08 -0700

    Adds initial sidebar and navbar components without styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'togla-ui';
+  title = 'Project Togla';
+  subtitle = 'Let\'s hope this works...'
 }
